4463fa5c3075786d081ae572816dbfad1439ed29
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2024-10-30 18:02:18 -0400

    Add files via upload
    Added Corwin Bot Install Package CMD.

---
+++ Corwin Bot Install Packages.cmd
@@ -0,0 +1,53 @@
+@echo off
+echo Installing dependencies...
+
+npm install @discordjs/voice
+npm install @haileybot/captcha-generator
+npm install @iamtraction/google-translate
+npm install @top-gg/sdk
+npm install axios
+npm install canvacord
+npm install chalk
+npm install cheerio
+npm install discord-giveaways
+npm install discord-image-generation
+npm install discord.js
+npm install dompurify
+npm install dotenv
+npm install erela.js
+npm install erela.js-apple
+npm install erela.js-deezer
+npm install erela.js-facebook
+npm install erela.js-spotify
+npm install express
+npm install figlet
+npm install fs
+npm install generate-password
+npm install giphy-api
+npm install hdqwalls-wrapper
+npm install he
+npm install html-entities
+npm install i
+npm install is-hexcolor
+npm install isgd
+npm install jsdom
+npm install libsodium-wrappers
+npm install lyrics-finder
+npm install mathjs
+npm install moment
+npm install moment-duration-format
+npm install moment-timezone
+npm install mongoose
+npm install ms
+npm install node-fetch
+npm install parse-ms
+npm install popcat-wrapper
+npm install request
+npm install sourcebin
+npm install topgg-autoposter
+npm install util
+npm install weather-js
+npm install ytdl-core
+
+echo Dependencies installed successfully.
+pause
